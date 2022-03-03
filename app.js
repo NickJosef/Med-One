d8baaf89f2e4cb8869646dc68edbf3fd41345f5c
NickJosef <100837361+NickJosef@users.noreply.github.com>
2022-03-03 16:19:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
