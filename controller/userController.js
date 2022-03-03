aeb41b55ca185be6ae997cf2859574bae9165f3e
NickJosef <100837361+NickJosef@users.noreply.github.com>
2022-03-03 16:40:20 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -33,11 +33,12 @@ exports.login = (req, res, next) => {
           expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
           ),
-          httpOnly: true,
+          httpOnly: false,
         })
         .json({
           status: "success",
           data: result,
+       
         });
     }
   });
